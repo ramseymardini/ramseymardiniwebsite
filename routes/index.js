8dc4aa5f98489ab4f812f8c9129d69eb9a60b8cf
Ramsey Mardini <ramseymardini@berkeley.edu>
2018-12-22 11:55:21 -0800

    Put a temporary page for my website

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Ramsey' });
+  res.render('index', { title: 'Ramsey Mardini\'s Website' });
 });
 
 module.exports = router;

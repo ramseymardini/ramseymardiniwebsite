210b094688acc08991eb7b27ac61b737e3fa4351
Ramsey Mardini <ramseymardini@gmail.com>
2018-12-30 14:49:49 -0800

    Started to add other pages and removed search bar

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Ramsey Mardini\'s Website' });
 });
 
+router.get('/about', function(req, res){
+  res.render('about', {
+    title: 'About Me'
+  });
+});
+
+router.get('/links', function(req, res){
+  res.render('links', {
+    title: 'Links + Github'
+  });
+});
+
 module.exports = router;

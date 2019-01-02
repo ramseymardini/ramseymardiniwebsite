0adbe015d2dd5ec4f99d140e8f0a24754acb6c86
Ramsey Mardini <ramseymardini@gmail.com>
2019-01-02 12:47:07 -0800

    Changed Placeholder message

--- routes/index.js
+++ routes/index.js
@@ -18,4 +18,27 @@ router.get('/links', function(req, res){
   });
 });
 
+router.get('/projects/vaporvoid', function(req, res){
+  res.render('projects/vaporvoid', {
+    title: 'VaporVoid'
+  });
+});
+
+router.get('/projects/bluebeard', function(req, res){
+  res.render('bluebeard', {
+    title: 'THe Tale of Bluebeard'
+  });
+});
+
+router.get('/projects/website', function(req, res){
+  res.render('website', {
+    title: 'This Website'
+  });
+});
+
+router.get('/projects/other_projects', function(req, res){
+  res.render('other_projects', {
+    title: 'Other Projects'
+  });
+});
 module.exports = router;
